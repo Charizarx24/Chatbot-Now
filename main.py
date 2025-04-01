84ab1143c7ad332da02f26747ecff9326f99acd5
Charizarx24 <134432997+Charizarx24@users.noreply.github.com>
2025-04-01 15:40:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -75,5 +75,5 @@ async def buscar_farmacias(
     return {
         "estado": estado_normalizado,
         "producto": producto_normalizado,
-        "farmacias": resultado if resultado else False
+        "farmacias": "\n".join([f"• {f}" for f in resultado]) if resultado else False
     }
